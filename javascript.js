98a9dce6f8bfbe7eaaac5654690a652a7be714f1
dzmagiorga <[200~184104905+dzmagiorga@users.noreply.github.com~>
2026-01-23 22:56:22 +0400

    make text output functions and playRount()

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ getComputerChoice: done
 
 getHumanChoice: done
 
-humanScore and computerScore: 
+humanScore and computerScore: done
 
 playRound: 
 
@@ -23,7 +23,7 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let choice = prompt("Please enter your choice - rock, paper or scissors:")
+    let choice = prompt("Please enter your choice - rock, paper or scissors:").toLowerCase();
     if (choice === "rock" || choice === "paper" || choice === "scissors") return choice;
     else if (choice === null) return "rock";
     else {
@@ -32,5 +32,43 @@ function getHumanChoice(){
     }
 }
 
+let computerScore = 0;
+let humanScore = 0;
 
+function compWinText(humanChoice, computerChoice){
+    console.log(`You chose ${humanChoice}, I chose ${computerChoice}, I WIN!!! >:D`);
+}
 
+function compLoseText(humanChoice, ComputerChoice){
+    console.log(`You chose ${humanChoice}, I chose ${computerChoice}.. I lose :/  :(`);
+}
+
+function drawText(humanChoice){
+    console.log(`You chose ${humanChoice}, so did I, it's a draw :|`);
+}
+
+function playRound(){
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    if (humanChoice === computerChoice){
+        drawText(humanChoice);
+        return;
+    }
+    if ( (humanChoice === "rock" && computerChoice === "scissors") ||
+         (humanChoice === "scissors" && computerChoice === "paper") ||
+         (humanChoice === "paper" && computerChoice === "rock" )
+    ){
+        compLoseText(humanChoice, computerChoice);
+        computerScore++;
+        return;
+    }
+    if ( (humanChoice === "rock" && computerChoice === "paper") ||
+         (humanChoice === "paper" && computerChoice === "scissors") ||
+         (humanChoice === "scissors" && computerChoice === "rock")
+    ){
+        compWinText(humanChoice, computerChoice);
+        computerScore++;
+        return;
+    }
+}
