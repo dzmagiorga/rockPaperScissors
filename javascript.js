7df7da5695ae9514512842358cf98783b908fc3f
dzmagiorga <[200~184104905+dzmagiorga@users.noreply.github.com~>
2026-01-23 21:51:23 +0400

    Make the function getHumanChoice()

--- javascript.js
+++ javascript.js
@@ -2,15 +2,15 @@
 
 we need:
 
-getComputerChoice:
+getComputerChoice: done
 
-getHumanChoice:
+getHumanChoice: done
 
-humanScore and computerScore:
+humanScore and computerScore: 
 
-playRound:
+playRound: 
 
-playGame:
+playGame: 
 
 */
 
@@ -21,4 +21,16 @@ function getComputerChoice(){
     if (1 <= choice && choice < 2) return "paper";
     if (2 <= choice && choice < 3) return "scissors";
 }
+
+function getHumanChoice(){
+    let choice = prompt("Please enter your choice - rock, paper or scissors:")
+    if (choice === "rock" || choice === "paper" || choice === "scissors") return choice;
+    else if (choice === null) return "rock";
+    else {
+        alert("you have to choose EXACTLY one of the three,\ntry again...");
+        return getHumanChoice();
+    }
+}
+
+
 
